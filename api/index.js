48189cfa56ff2fe235233da0c60579e9bb063571
Rafly Ardiansyah <rafly21ardiansyah@gmail.com>
2024-04-09 19:16:29 +0700

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
+import { Console, log } from "console";
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const port = 3000;
 const app = express();
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("database is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
 });
