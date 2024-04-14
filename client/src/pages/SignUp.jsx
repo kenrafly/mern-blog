9bd0f1581d16fc9b44f89e8cf4892e8d4be817f9
Rafly Ardiansyah <rafly21ardiansyah@gmail.com>
2024-04-14 16:36:24 +0700

    add OAth functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import OAuth from "../components/OAuth";
 const SignUp = () => {
   const [formData, setFormData] = useState({});
   const navigate = useNavigate();
@@ -96,6 +97,7 @@ const SignUp = () => {
                 "Sign Up"
               )}
             </Button>
+            <OAuth />
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span> Have an account?</span>
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,6 +3,7 @@ import userReducer from "./user/userSlice.js";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import persistStore from "redux-persist/es/persistStore";
+
 const rootReducer = combineReducers({ user: userReducer });
 const persistConfig = { key: "root", storage, version: 1 };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
