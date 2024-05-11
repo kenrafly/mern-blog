9d8fbbb254872e0bd71b57c0bb29396891a5e056
Rafly Ardiansyah <rafly21ardiansyah@gmail.com>
2024-05-11 08:52:59 +0700

    complete update user profie page functionality

--- api/index.js
+++ api/index.js
@@ -6,28 +6,28 @@ import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
-const port = 3000;
-const app = express();
 
-app.use(express.json());
-app.use(cookieParser());
-//connect to the database
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log("database is connected");
+    console.log("MongoDb is connected");
   })
   .catch((err) => {
     console.log(err);
   });
-app.listen(port, () => {
-  console.log(`server is running on port ${port}`);
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!");
 });
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
-//middleware to handle the error
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,6 +3,8 @@ import { test, updateUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
+
 router.get("/test", test);
 router.put("/update/:userId", verifyToken, updateUser);
+
 export default router;
